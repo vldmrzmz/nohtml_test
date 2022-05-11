efd87555c0307d5678e1cdca50381b03eb5fca51
vldmrzmz <vladimirzomarev01@gmail.com>
2022-05-12 01:07:48 +0300

    1
    2

--- src/functions.js
+++ src/functions.js
@@ -1,21 +1,5 @@
-function newQuestion(quest, [ans1, ans2, ans3], correct){
-    this.question = quest;
-    this.ans1 = ans1;
-    this.ans2 = ans2;
-    this.ans3 = ans3;
+export function NewQuestion(quest, ansArr = [], correct){
+    this.quest = quest;
+    this.ansArr = ansArr;
     this.correct = correct;
-}
-function renderRadio(radio, list, label, answerText) {
-    radio = document.createElement('input');
-    radio.setAttribute('name', 'rbutton');
-    radio.type = 'radio';
-    let li = document.createElement('li');
-    label = document.createElement("label");
-    label.innerHTML = answerText;
-    label.className = "form-radiolist__answer"
-    li.innerHTML = radio.outerHTML + label.outerHTML;
-    list.appendChild(li);
-}
-function getFormValue(event) {
-    event.preventDefault();
 }
--- src/main.js
+++ src/main.js
@@ -1,26 +1,45 @@
 import './style.scss';
-// import newQuestion from './functions'; export nado
-// import renderRadio from './functions';
-// import getFormValue from './functions';
-function NewQuestion(quest, ansArr = [], correct){
-    this.quest = quest;
-    for (let i = 0; i < ansArr.length; i++) {
-        ansArr[i] = ansArr[i];
-    }
-    this.correct = correct;
-}
-// function renderRadio(radio, list, label, answerText) {
-//     radio = document.createElement('input');
-//     radio.setAttribute('name', 'rbutton');
-//     radio.type = 'radio';
-//     let li = document.createElement('li');
-//     label = document.createElement("label");
-//     label.innerHTML = answerText;
-//     label.className = "form-radiolist__answer"
-//     li.innerHTML = radio.outerHTML + label.outerHTML;
-//     list.appendChild(li);
-// }
+import {NewQuestion} from './functions'; 
+// import {validate} from './functions';
 var quantity = 0;
+var rightCounter = 0;
+function validate (arr, rightArr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == rightArr[i]){
+            rightCounter++;
+        }
+    }
+}
+function renderFinal() {
+    form.innerHTML = "";
+    form.className = "final-form"
+    let h1 = document.createElement('h1');
+    h1.className = "final-form__h1";
+    h1.textContent = "Ваши результаты: "
+    form.appendChild(h1);
+    console.log(rightCounter + rightCounter.type);
+    
+    let p2 = document.createElement('p');
+    p2.className = "final-form__p2";
+    p2.innerHTML = `Всего вопросов: ${quantity}`;
+    form.appendChild(p2);
+
+    let p = document.createElement('p');
+    p.className = "final-form__p";
+    p.innerHTML = `Правильных ответов: ${rightCounter}`;
+    form.appendChild(p);
+
+    let p3 = document.createElement('p');
+    p3.className = "final-form__p3";
+    p3.innerHTML = `Процент успеха: ≈${rightCounter / quantity * 100} %`;
+    form.appendChild(p3);
+
+    let finalBtn = document.createElement('a');
+    finalBtn.href = "";
+    finalBtn.innerHTML = "Пройти еще раз"
+    finalBtn.className = "final-form__btn"
+    form.appendChild(finalBtn);
+}
 function renderQuestion(questionObj, form) {
     let qText = document.createElement('p');
     qText.className = "form__qText";
@@ -30,73 +49,71 @@ function renderQuestion(questionObj, form) {
     let radioList = document.createElement('ul');
     radioList.className = "form-radiolist";
     form.appendChild(radioList);
-    for (let i = 0; i < 3; i++) {
-        let radio = document.createElement('input');
-        radio.setAttribute('name', quantity);
-        radio.type = 'radio';
+    for (let i = 0; i < questionObj.ansArr.length; i++) {
+        let input = document.createElement('input');
+        input.setAttribute('value', questionObj.ansArr[i])
+        input.setAttribute('name', quantity);
+        const idAtribute =  `${questionObj.ansArr[i]}-${quantity}`;
+        input.setAttribute('id', idAtribute)
+        input.type = 'radio';
         let li = document.createElement('li');
         let label = document.createElement("label");
-        label.innerHTML = questionObj.ansArr[i];//cannot read undefined property
+        label.setAttribute('for', idAtribute);
+        label.innerHTML = questionObj.ansArr[i]; //cannot read undefined property
         label.className = "form-radiolist__answer"
-        li.innerHTML = radio.outerHTML + label.outerHTML;
+        li.innerHTML = input.outerHTML + label.outerHTML;
         radioList.appendChild(li);
     }
     quantity++;
 }
-function getFormValue(event) {
-    event.preventDefault();
-}
-
 //  ===============main==============
-var firstQuestion = new NewQuestion(
-"Какого типа данных нету в JavaScript?", ["var", "float", "let"], "float");
-console.log(firstQuestion);
-
-// var questionText1 = document.createElement('h1');
-// questionText1.className = "question-text";
-// questionText1.innerHTML = firstQuestion.question;
-// document.body.appendChild(questionText1);
-
+var validArr = [];
 var form = document.createElement('form');
 form.className = "form";
 document.body.appendChild(form);
 
-renderQuestion(firstQuestion, form);
+var question1 = new NewQuestion(
+"1. Какого типа данных нету в JavaScript?", ["var", "float", "let"], "float");
+validArr.push(question1.correct);
 
+renderQuestion(question1, form);
 
-// var answer1;
-// var answerVariant;
-// renderRadio(answer1, radioList, answerVariant, firstQuestion.ans1);
-// var answer2;
-// renderRadio(answer2, radioList, answerVariant, firstQuestion.ans2);
-// var answer3;
-// renderRadio(answer3, radioList, answerVariant, firstQuestion.ans3);
+var question2 = new NewQuestion(
+"2. Самый популярный фреймворк JS?", ["React", "Node", "Ember"], "React");
+validArr.push(question2.correct);
+renderQuestion(question2, form);
 
-var button = document.createElement('a');
-button.href = '#';
+var question3 = new NewQuestion(
+"3. Какого логического оператора не сущеструет?", ["||", "&&", "??", "$$"], "$$");
+validArr.push(question3.correct);  
+renderQuestion(question3, form);
+
+var question4 = new NewQuestion("4. Какая из этих команд работает не так как другие две?",
+["elem.append(document.createTextNode(text))","elem.innerHTML = text",
+"elem.textContent = text", ], "elem.innerHTML = text");
+validArr.push(question4.correct);
+renderQuestion(question4, form);
+
+var question5 = new NewQuestion("5. Какая переменная записана неверно?",
+["var isDone = 0;","var num = 'STRING';",
+"var number = 12,5;", ], "var number = 12,5;");
+validArr.push(question5.correct);
+renderQuestion(question5, form);
+
+var button = document.createElement('button');
 button.className = "form__button";
 button.innerHTML = "Отправить";
-button.type = "submit"
+button.type = "submit";
 form.appendChild(button);
 
-// button.addEventListener('click', () => nextClick(answerVariant, firstQuestion.correct));
 
-
-
-
-
-// var questionAnswer1 = document.createElement('input');
-// questionAnswer1.type = 'radio';
-// questionAnswer1.innerHTML = q1.ans1;
-// document.body.appendChild(questionAnswer1);
-
-// var questionAnswer2 = document.createElement('input');
-// questionAnswer2.type = 'radio';
-// questionAnswer2.innerHTML = q1.ans2;
-// document.body.appendChild(questionAnswer2);
-
-// var questionAnswer3 = document.createElement('input');
-// questionAnswer3.type = 'radio';
-// questionAnswer3.innerHTML = q1.ans3;
-// document.body.appendChild(questionAnswer3);
-
+document.querySelector('form').addEventListener('submit', (e) => {
+    const formData = new FormData(form);
+    e.preventDefault();
+    var dataArr = [];
+    for (let i = 0; i < quantity; i++) {
+        dataArr.push(formData.getAll(`${i}`));
+    }
+    validate(dataArr, validArr);
+    renderFinal();
+});
